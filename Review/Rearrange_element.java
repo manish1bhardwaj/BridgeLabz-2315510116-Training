bf9f522a84206a206389088e9742f49b640b4f45
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2025-09-01 11:31:33 +0530

    Adding new file

---
+++ Review/Rearrange_element.java
@@ -0,0 +1,33 @@
+package arrays;
+import java.util.*;
+
+public class Rearrange_element {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, -4, -1, 4, -6, -7,9,8};
+        int[] result = rearrange(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] rearrange(int[] arr) {
+        int[] pos = new int[arr.length];
+        int[] neg = new int[arr.length];
+        int p = 0, n = 0;
+        for (int num : arr) {
+            if (num >= 0) pos[p++] = num;
+            else neg[n++] = num;
+        }
+        int[] result = new int[arr.length];
+        int i = 0, pi = 0, ni = 0;
+        while (pi < p && ni < n) {
+            result[i++] = pos[pi++];
+            result[i++] = neg[ni++];
+        }
+        while (pi < p) {
+            result[i++] = pos[pi++];
+        }
+        while (ni < n) {
+            result[i++] = neg[ni++];
+        }
+        return result;
+    }
+}
